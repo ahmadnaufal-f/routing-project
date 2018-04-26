bf9ee7bb910fb3327e801163c31880266f4e93b1
radityaevans <38757096+radityaevans@users.noreply.github.com>
2018-04-26 22:09:59 +0900

    Add files via upload

---
+++ Cli.java
@@ -0,0 +1,22 @@
+package routing;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+public class Cli {
+
+	
+	public static void main(String args[]) throws UnknownHostException, IOException{
+	int number;
+	Scanner scan = new Scanner(System.in);	
+	Socket sc = new Socket("127.0.0.1",323);
+	Scanner scan1 = new Scanner(sc.getInputStream());
+	System.out.println("Enter any number");
+	number = scan.nextInt();
+	PrintStream p = new PrintStream(sc.getOutputStream());
+	
+		
+	}
+	
+}
