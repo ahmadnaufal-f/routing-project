a520a25fce8e1546d0da6d4d3bdac58d19d80515
ivanasalim <ivanalemmuelasalim@gmail.com>
2018-04-30 23:17:53 +0900

    Update MainDijkstra.java

--- routing/src/routing/MainDijkstra.java
+++ routing/src/routing/MainDijkstra.java
@@ -1,5 +1,6 @@
 package routing;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -9,55 +10,110 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		int AdjacencyMatrix[][];
 		int nodeNum =5;
-		int source=0, destination=0; //initialize the source and destination to be 0 first
-		
-		
+		char sourceName;
+		char destinationName; //initialize the source and destination to be 0 first
+		int source =0, destination =0;
+		ArrayList<Integer> path = new ArrayList<Integer>();
            
-            AdjacencyMatrix = new int[6][6];
- 
-            for (int i = 1; i <= nodeNum; i++)
-            {
-                for (int j = 1; j <=nodeNum; j++)
-                {
-
-                   
-                    if (i == j)
-                    {
-     
-                    	AdjacencyMatrix[i][j] = 0;
-                        System.out.printf("The edge for the matrix for the source of %d to the destination of %d is 0", i, j);
-                        System.out.println();
-                      
-                    }
-                    else
-                    {
-                    	 System.out.printf("Enter the cost of the edge for the matrix for the source of %d to the destination of %d : ", i, j);
-                         AdjacencyMatrix[i][j] = sc.nextInt();
-                    }
-                   
-                }
-            }
+		AdjacencyMatrix = new int[6][6];
+		Computation dijkstra = new Computation(nodeNum);
+        dijkstra.initializeEdge(AdjacencyMatrix);
+          
+            
+            
  
             do {
             System.out.println("Enter the source node: ");
-            source = sc.nextInt();
-            }while(source<1 || source>5);
+            sourceName = sc.next().charAt(0);
+            }while(sourceName!='A' && sourceName!='B' && sourceName!='C' && sourceName!='D' && sourceName!='E');
  
             do {
             System.out.println("Enter the destination node: ");
-            destination = sc.nextInt();
-            }while(destination<1 || destination>5);
+            destinationName = sc.next().charAt(0);
+            }while(destinationName!='A' && destinationName!='B' && destinationName!='C' && destinationName!='D' && destinationName!='E');
  
-            Computation dijkstra = new Computation(nodeNum);
-            dijkstra.algorithm(AdjacencyMatrix, source);
+           
+            
+            
+            
+            if(sourceName =='A')
+            {
+            	source =1;
+            }
+            else if(sourceName == 'B')
+            {
+            	source =2;
+            }
+            else if(sourceName == 'C')
+            {
+            	source =3;
+            }
+            else if(sourceName == 'D')
+            {
+            	source =4;
+            }
+            else if(sourceName == 'E')
+            {
+            	source =5;
+            }
+            
+            
+            
+            
+            
+            if(destinationName =='A')
+            {
+            	destination =1;
+            }
+            else if(destinationName == 'B')
+            {
+            	destination =2;
+            }
+            else if(destinationName == 'C')
+            {
+            	destination =3;
+            }
+            else if(destinationName == 'D')
+            {
+            	destination =4;
+            }
+            else if(destinationName == 'E')
+            {
+            	destination =5;
+            }
+           
  
+            dijkstra.algorithm(source, path);
             
             for (int i = 1; i <= dijkstra.distance.length-1; i++) //subtract by 1 because initially distance[node] is 0
             {
                 if (i == destination)
-                    System.out.println("The shortest path from" + source + " to " + i + " is "  //to find the destination in the loop to calculate 
+                    System.out.println("The shortest path from " + sourceName + " to " + destinationName + " is "  //to find the destination in the loop to calculate 
                     						//the formula is the current source added by the distance to the destination
                             + dijkstra.distance[i]);
+                
+                
+            }
+           
+            System.out.println();
+            System.out.println("The path for this is: ");
+            for(int i=0; i<path.size(); i++)
+            {
+            	
+            	if(path.get(i) == 1)
+            		System.out.println('A');
+            	if(path.get(i) == 2)
+            		System.out.println('B');
+            	if(path.get(i) == 3)
+            		System.out.println('C');
+            	if(path.get(i) == 4)
+            		System.out.println('D');
+            	if(path.get(i)==5)
+            		System.out.println('E');
+            	if(path.get(i) == destination)
+            	{
+            		break;
+            	}
             }
         
         
