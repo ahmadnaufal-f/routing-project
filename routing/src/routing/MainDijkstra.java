c14faaecba017d47c77d0c07fa3ee8581135b026
ivanasalim <ivanalemmuelasalim@gmail.com>
2018-04-30 23:38:32 +0900

    Update MainDijkstra.java

--- routing/src/routing/MainDijkstra.java
+++ routing/src/routing/MainDijkstra.java
@@ -3,7 +3,7 @@ package routing;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class MainDijkstra {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
