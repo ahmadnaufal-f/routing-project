53e258f497a96b632f0b19e4a5627d27709e453a
ivanasalim <ivanalemmuelasalim@gmail.com>
2018-04-30 23:11:33 +0900

    Update Computation.java

--- routing/src/routing/Computation.java
+++ routing/src/routing/Computation.java
@@ -1,6 +1,7 @@
 package routing;
 
 import java.util.HashSet; //this hashset is used for storage, can be accessed through a key
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -10,43 +11,97 @@ public class Computation {
 	private Set<Integer> settled; //settled because the nodes are already traveled to from the source in finding the shortest distance.
 	private Set<Integer> unsettled; //unsettled because these nodes are not yet traveled to from the source in finding the shortest distance.
 	private int nodesNum = 5;
+	public ArrayList<Integer> order = new ArrayList<Integer>(); //to store the order of the path
 	
-	public Computation(int nodesNumb) //constructor, this is the initialization
+	
+	public Computation(int nodesNum) //constructor, this is the initialization
 	{
-		nodesNumb = nodesNum; //there are 5 hosts so number of nodes will be 5
-		distance = new int[6];
+		this.nodesNum = nodesNum; //there are 5 hosts so number of nodes will be 5
+		distance = new int[6];//there are 5 hosts, needs 6 arrays since the last one is used to store null
 		settled = new HashSet<Integer>();
 		unsettled = new HashSet<Integer>();
 		adjacencyMatrix = new int[6][6];  //6 because total number of nodes are 5, added by 1 because it is used to store null at the end
+		
+		
 	}
 	
-	public void algorithm(int AdjacencyMatrix[][], int source)
+	
+	
+	public void initializeEdge(int AdjacencyMatrix[][]) //define our own edges
 	{
-		int calculateNode; //this will be the node that will be processed, after it is processed, it will be placed in the settled sets instead of unsettled
-		
-		for (int i = 1; i <= 5; i++) 
+		for(int i=1; i<=5; i++)
 		{
-			for(int j =1; j<=5; j++)
+			for(int j=1; j<=5; j++)
 			{
-				adjacencyMatrix[i][j] = AdjacencyMatrix[i][j]; //since this function requires the input of the adjacency matrix and source
-													//the adjacency matrix in the input and this function must be fitted
+				if(i==1 && j==2)
+				{
+					AdjacencyMatrix[1][2] = 15;
+				}
+				else if(i == 1 && j ==3)
+				{
+					AdjacencyMatrix[1][3] = 8;
+				}
+				else if(i == 2 && j ==3)
+				{
+					AdjacencyMatrix[2][3] = 4;
+				}
+				else if(i == 2 && j ==4)
+				{
+					AdjacencyMatrix[2][4] = 21;
+				}
+				else if(i == 3 && j ==4)
+				{
+					AdjacencyMatrix[3][4] = 10;
+				}
+				else if(i == 3 && j ==5)
+				{
+					AdjacencyMatrix[3][5] = 2;
+				}
+				else if(i == 4 && j ==5)
+				{
+					AdjacencyMatrix[4][5] = 17;
+				}
+				else
+					AdjacencyMatrix[4][5] = 999;
+				
+			
+				
+				adjacencyMatrix[i][j] = AdjacencyMatrix[i][j];
 			}
 		}
 		
+	
+		
+	}
+	
+	
+	
+	
+	
+	public void algorithm(int source, ArrayList<Integer> ordered)
+	{
+		int calculateNode; //this will be the node that will be processed, after it is processed, it will be placed in the settled sets instead of unsettled
+		
+		
+		
+		order = ordered;
+		
 		for(int i =1; i<=5; i++)
 		{
-			// System.out.println(i);
-			distance[i] = Integer.MAX_VALUE; //make all the distance in each node infinity so that it can be compared later on to find the minimum distance.
+			
+			distance[i] = 999; //make all the distance in each node so great so that it can be compared later on to find the minimum distance.
 		}
 		
 		unsettled.add(source); //source is added in this list because it is needed to find the minimum distance from the current source
-		distance[source] = 0; //distance from current source to the next neighbor node is 0 initially since we haven't calculate who are the neighbors
+		distance[source] = 0; //distance from cAurrent source to the next neighbor node is 0 initially since we haven't calculate who are the neighbors
 		while(!unsettled.isEmpty())
 		{
 			calculateNode = calculateNeighborToFindMinimum(); //this is to find the next node, with the minimum distance, from the current node in the unsettled.
 			unsettled.remove(calculateNode);
 			settled.add(calculateNode); //after it is settled (finding the next node in the path, the node will be put in the settled list
+			
 			nextNode(calculateNode); //now it is time to assess the neighbor of the current node
+			System.out.println(calculateNode);
 		}
 	}
 
@@ -69,10 +124,12 @@ public class Computation {
 						//when run at first in order to initialize the minimum
 					//however, the initialized minimum distance will be compared with other distances later on to find the final minimum
 					node =i; //finally the minimum distance is found, this node will be the one that will be traveled on next	
+					
 				}
 			}
 			
 		}
+		order.add(node);
 		
 		return node;
 	}
@@ -104,6 +161,7 @@ public class Computation {
 				}
 			}
 		}
+		
 	}
 	
 	
